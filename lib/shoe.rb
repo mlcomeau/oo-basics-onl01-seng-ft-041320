7e8bc430d077b11676b2e15407a49dea0e135493
Megan Comeau <mlcomeau99@gmail.com>
2020-04-16 19:31:47 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,20 +1,20 @@
-class Shoe 
+class Shoe
   def initialize(brand)
-    @brand = brand 
+    @brand = brand
   end
 
-  def brand 
+  def brand
     @brand
   end
 
   def color=(color)
-    @color = color 
+    @color = color
   end
 
-  def color 
-    @color 
+  def color
+    @color
   end
 
-  
-  
+
+
 end
